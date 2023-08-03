c96bbaf71d1df645626376071668cdd23cf5319d
betiniakarandut <betiniakarandut@gmail.com>
2023-08-03 22:31:32 +0100

    Change: float to int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-"""Module to implement floor function"""
+"""Module to implement 'floor' function"""
 import math
 
 
-def floor(n: float) -> float:
-    """Returns floor of float."""
+def floor(n: float) -> int:
+    """
+    Returns floor of float
+
+    Args:
+        n(float): to check
+    """
     return math.floor(n)
