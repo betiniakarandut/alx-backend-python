b6ad5478abda58c6e2adbf2007443c3d97325c40
betiniakarandut <betiniakarandut@gmail.com>
2023-08-03 21:56:33 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+# #!/usr/bin/env python3
+"""Module to implement 'floor' function"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Returns floor of float
+
+    Args:
+        n(float): to check
+    """
+    return math.floor(n)
