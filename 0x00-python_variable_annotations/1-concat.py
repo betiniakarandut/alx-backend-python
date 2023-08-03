c6c82818d16eecf56a78bb3610657ab9c80f7876
betiniakarandut <betiniakarandut@gmail.com>
2023-08-03 21:29:43 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for function concat
+"""
+
+if __name__ == "__main__":
+
+    def concat(str1: str, str2: str) -> str:
+        """Concat two strings"""
+        return str1 + str2
+
+
+# str1 = "egg"
+# str2 = "shell"
+
+# print(concat(str1, str2) == "{}{}".format(str1, str2))
+# print(concat.__annotations__)
