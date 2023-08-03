4ed0894bbf58238c71f1a588d39086c2eb609edb
betiniakarandut <betiniakarandut@gmail.com>
2023-08-03 22:55:32 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+# #!/usr/bin/env python3
+"""3-to_str.py Module"""
+
+
+def to_str(n: float) -> float:
+    """function for string repr
+
+    Args:
+        n(float): float to check
+
+    Returns:
+         string representation of float
+    """
+    return n.__repr__()
