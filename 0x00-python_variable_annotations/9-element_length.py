37fa1b50a27cc9f2239ba2c21cac81eb6d666d54
betiniakarandut <betiniakarandut@gmail.com>
2023-08-04 02:57:34 +0100

    Undo: #

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
-# #!/usr/bin/env python3
+#!/usr/bin/env python3
 """9-element_length.py module"""
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns values with appropriate types"""
     return [(i, len(i)) for i in lst]
