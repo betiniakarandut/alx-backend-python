1c211995c04b7c19c85746769342ba176977421a
betiniakarandut <betiniakarandut@gmail.com>
2023-08-04 02:40:14 +0100

    8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""8-make_multiplier.py module"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns the child function"""
+    def multiplier_a(a: float) -> float:
+        """Returns the result of the multiplier"""
+        return a * multiplier
+    return multiplier_a
