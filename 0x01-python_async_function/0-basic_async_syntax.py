9b8e341aba71b884d5e90d255653e6dbfd43a3bc
betiniakarandut <betiniakarandut@gmail.com>
2023-08-07 22:39:45 +0100

    Included: + 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,5 +12,5 @@ async def wait_random(max_delay: float | int = 10) -> float:
     Returns:
         waits for max_delay time and some random floats
     """
-    wait_time = uniform(0, max_delay)
+    wait_time = uniform(0, max_delay + 1)
     return wait_time
