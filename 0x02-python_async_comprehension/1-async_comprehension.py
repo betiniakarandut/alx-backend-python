428626f330e70d352a76f1e7b5e84ce4aabc65eb
betiniakarandut <betiniakarandut@gmail.com>
2023-08-09 00:34:28 +0100

    Documentation: function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,14 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> AsyncGenerator:
+    """Coroutine function
+
+    Arg:
+        Takes no arguments
+
+    Returns:
+        async generator
+    
+    """
     result = [i async for i in async_generator()]
     return result
-
-# async def main():
-#     print(await async_comprehension())
-
-# asyncio.run(main())
