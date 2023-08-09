b210c28555b24f65f2d562aae5edca423f2682d8
betiniakarandut <betiniakarandut@gmail.com>
2023-08-09 20:52:25 +0100

    Return: total time taken

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,11 +11,3 @@ async def measure_runtime() -> float:
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
     return end_time - start_time
-
-
-async def main():
-    return await(measure_runtime())
-
-print(
-    asyncio.run(main())
-)
