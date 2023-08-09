dd083c8a3be40edc3d2b64cfdc233e20d340c705
betiniakarandut <betiniakarandut@gmail.com>
2023-08-09 01:30:44 +0100

    Refactor: List[float]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """1-async_comprehension.py module"""
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Coroutine function
 
     Arg:
