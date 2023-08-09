ad9c58f685f491403f397d68a52cd5284c230014
betiniakarandut <betiniakarandut@gmail.com>
2023-08-09 01:22:04 +0100

    Update: AsyncGenerator[float, None, None]

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Coroutine function that loops 10 times.
 
     Args:
